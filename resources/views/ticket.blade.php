55a26b59235ef8d3f60b8198570b01e9058ff943
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2018-01-31 20:20:34 +0700

    Change title

--- resources/views/ticket.blade.php
+++ resources/views/ticket.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="css/app.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>IT Ambassador | 2017</title>
+    <title>IT3KINGS 15th</title>
 </head>
 <body>
     <div id="app">
